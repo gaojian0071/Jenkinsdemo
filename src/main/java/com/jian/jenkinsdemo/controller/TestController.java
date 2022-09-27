c618c46f3a156d1f1f69b8c501ddcc50f53d249e
gaojian <1942593530@qq.com>
2022-09-27 15:35:56 +0800

    third

--- src/main/java/com/jian/jenkinsdemo/controller/TestController.java
+++ src/main/java/com/jian/jenkinsdemo/controller/TestController.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping("/")
 public class TestController {
 
     @GetMapping("/test")
     public String test(){
-        return "hello world！";
+
+        return "hello world！"+ LocalDateTime.now();
     }
 }
